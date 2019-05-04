78e9c88e76e8fd86f6058d4ec59475c7fb53408e
Rodrigo S. Rahman de Almeida <rodrigo.almeida@jfive.com.br>
2019-05-04 12:51:21 -0300

    correcao de login

--- lib/pages/FirstAccess.dart
+++ lib/pages/FirstAccess.dart
@@ -61,111 +61,113 @@ class _FirstAccessState extends State<FirstAccess> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("images/first.jpg"),
-              fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("images/first.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Align(
-            alignment: Alignment.center,
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Image.asset('images/logo_branco.png'),
-                      ),
-                      ErrorMessage(errorMessage),
-                      TextFormField(
-                        controller: loginController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white24,
-                            prefixIcon: Icon(Icons.person),
-                            hintText: 'E-mail',
-                            labelText: 'E-mail',
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide(width: 0))),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Login obrigatório';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                        obscureText: true,
-                        controller: senhaController,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white24,
-                            prefixIcon: Icon(Icons.lock),
-                            hintText: 'Senha',
-                            labelText: 'Senha',
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide(width: 0))),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Senha obrigatório';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        child: RaisedButton(
-                          onPressed: () {
-                            _login(context);
+          Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Image.asset('images/logo_branco.png'),
+                        ),
+                        ErrorMessage(errorMessage),
+                        TextFormField(
+                          controller: loginController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white24,
+                              prefixIcon: Icon(Icons.person),
+                              hintText: 'E-mail',
+                              labelText: 'E-mail',
+                              border: UnderlineInputBorder(
+                                  borderSide: BorderSide(width: 0))),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Login obrigatório';
+                            }
                           },
-                          child: Text('ENTRAR',
-                              style: TextStyle(color: Colors.white)),
-                          color: Color.fromRGBO(252, 117, 59, 1),
-                          shape: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            borderSide: BorderSide(
-                              color: Color.fromRGBO(252, 117, 59, 1),
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          obscureText: true,
+                          controller: senhaController,
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white24,
+                              prefixIcon: Icon(Icons.lock),
+                              hintText: 'Senha',
+                              labelText: 'Senha',
+                              border: UnderlineInputBorder(
+                                  borderSide: BorderSide(width: 0))),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Senha obrigatório';
+                            }
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child: RaisedButton(
+                            onPressed: () {
+                              _login(context);
+                            },
+                            child: Text('ENTRAR',
+                                style: TextStyle(color: Colors.white)),
+                            color: Color.fromRGBO(252, 117, 59, 1),
+                            shape: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(252, 117, 59, 1),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Divider(),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    ForgotPassword(),
-                              ));
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'Esqueceu a senha?',
-                            style: TextStyle(color: Colors.white),
+                        Divider(),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      ForgotPassword(),
+                                ));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Esqueceu a senha?',
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )),
-      ],
+              )),
+        ],
+      ),
     );
   }
 }
